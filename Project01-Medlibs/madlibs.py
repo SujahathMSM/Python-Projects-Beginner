46e1b8530611315d17137f98451e9c30d774a384
SujahathMSM <sujahathmhmd@gmail.com>
2023-02-18 14:31:05 +0530

    Adding the First Project

---
+++ Project01-Medlibs/madlibs.py
@@ -0,0 +1,15 @@
+#string concatenation
+# youtuber = "Sujahath"
+# print("Welcome to "+youtuber)
+# print("welcome to {}".format(youtuber))
+# print(f"Welcome to {youtuber}")
+
+adj = input("Adjective: ")
+verb1 = input("Verb1: ")
+verb2 = input("Verb2: ")
+famous_person = input("Famoue person: ")
+
+
+madlib = f"computer program is so {adj}! It makes me so excited all the time because I love to {verb1}. Stay hydrated and {verb2} likeyou are {famous_person}"
+
+print(madlib)
